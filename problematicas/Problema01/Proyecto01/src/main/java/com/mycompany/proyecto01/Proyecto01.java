7035fa1a81d681832502a774c95fb301c1d8867b
francisco torres <ft11056575@gmail.com>
2023-11-23 17:48:07 -0500

    secuencia de numueros impares

---
+++ problematicas/Problema01/Proyecto01/src/main/java/com/mycompany/proyecto01/Proyecto01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto01;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ problematicas/Problema05/Problema05/src/main/java/com/mycompany/problema05/Problema05.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.problema05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Problema05 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int tabla;
+        int operacion;
+        int contador = 1;
+        String cadena = "";
+        do {
+            operacion = tabla + contador; 
+            System.out.printf("");
+            contador = contador +0+3+5+7+9+11;
+        } while (contador <= 37);
+
+    }
+}
